6a984dc0548cf261a5d55aa7fd9cb85bb0cbc05a
Cristian Radulescu <cristian67@gmail.com>
2013-03-28 00:14:37 +0200

    Update product listings

--- woocommerce.php
+++ woocommerce.php
@@ -17,23 +17,24 @@
               <div class="art-post-cl"></div>
               <div class="art-post-cr"></div>
               <div class="art-post-cc"></div>
-              <div class="art-post-body">
+              <div class="art-post-body" style="padding: 0">
                 <div class="art-post-inner art-article">
-
-                  <h2 class="art-postheader">
-                    <?php the_title(); ?>
-                    <?php if ( $product->enable_dimensions_display()  && $product->has_weight()): ?>
-                      (<?php echo $product->get_weight() . ' ' . get_option('woocommerce_weight_unit'); ?>)
-                    <?php endif ?>
-                  </h2>
 
                   <div class="art-postcontent">
                       <!-- article-content -->
 
                       <table style="width: 100%">
                         <tr>
-                          <td style="width: 175px; border: none"><?php woocommerce_template_loop_product_thumbnail() ?></td>
-                          <td style="border: none">
+                          <td style="width: 20%; border: none"><?php woocommerce_template_loop_product_thumbnail() ?></td>
+                          <td style="width: 60%; border: none; vertical-align: middle">
+
+                            <h2 class="art-postheader">
+                              <?php the_title(); ?>
+                              <?php if ( $product->enable_dimensions_display()  && $product->has_weight()): ?>
+                                (<?php echo $product->get_weight() . ' ' . get_option('woocommerce_weight_unit'); ?>)
+                              <?php endif ?>
+                            </h2>
+
                             <?php
                             echo the_content();
 
@@ -50,32 +51,26 @@
 
                             <?php endif; ?>
                           </td>
+
+                          <td style="width: 20%; border: none; vertical-align: middle; text-align: center">
+                            <div class="">
+                                <p style="font-size: 14px;">Preț: <?php echo $product->get_price_html(); ?></p>
+
+                                <form action="<?php echo esc_url( $product->add_to_cart_url() ); ?>" method="post" enctype='multipart/form-data'>
+                                  <span class="art-button-wrapper">
+                                    <span class="l"> </span>
+                                    <span class="r"> </span>
+                                    <button type="submit" class="button art-button">Adaugă în coș</button>
+                                  </span>
+                                </form>
+                            </div>
+                          </td>
                         </tr>
                       </table>
 
                       <!-- /article-content -->
                   </div>
                   <div class="cleared"></div>
-
-                  <div class="art-postmetadatafooter">
-
-                      <table style="border: none; width: 100%;">
-                        <tr>
-                          <td style="border: none">
-                            <form action="<?php echo esc_url( $product->add_to_cart_url() ); ?>" method="post" enctype='multipart/form-data'>
-                              <span class="art-button-wrapper">
-                                <span class="l"> </span>
-                                <span class="r"> </span>
-                                <button type="submit" class="button art-button">Adaugă în coș</button>
-                              </span>
-                            </form>
-                          </td>
-                          <td class="right" style="border: none; text-align: right">
-                            <p style="font-weight: bold; font-size: 14px">Preț: <?php echo $product->get_price_html(); ?></p>
-                          </td>
-                        </tr>
-                      </table>
-                  </div>
 
                 </div>
 
